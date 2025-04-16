9d0c3e836fe36dfcffaafd79cd1ca1162bdff596
FrancescaBianchini <55796931+FrancescaBianchini@users.noreply.github.com>
2025-04-16 22:02:25 +0200

    Update customer_date_wizard.py

--- wizards/customer_date_wizard.py
+++ wizards/customer_date_wizard.py
@@ -2,26 +2,71 @@ from odoo import models, fields, api
 
 class CustomerDateWizard(models.TransientModel):
     _name = 'customer.date.wizard'
-    _description = 'Customer Filter by Date Range'
+    _description = 'Filter Customers by Documents in Date Range'
 
-    date_from = fields.Date(string="From", required=True)
-    date_to = fields.Date(string="To", required=True)
+    date_from = fields.Date(string="Start Date", required=True)
+    date_to = fields.Date(string="End Date", required=True)
+
+    line_ids = fields.One2many('customer.report.line', 'wizard_id', string="Risultati")
 
     def action_get_customers(self):
-        domain = ['|']
+        # Pulizia precedente
+        self.line_ids.unlink()
 
-        domain += [('invoice_ids.invoice_date', '>=', self.date_from),
-                   ('invoice_ids.invoice_date', '<=', self.date_to)]
+        AccountMove = self.env['account.move']
+        SaleOrder = self.env['sale.order']
 
-        domain += [('sale_order_ids.date_order', '>=', self.date_from),
-                   ('sale_order_ids.date_order', '<=', self.date_to)]
+        # Fatture cliente non annullate nel range
+        invoices = AccountMove.search([
+            ('move_type', '=', 'out_invoice'),
+            ('state', '!=', 'cancel'),
+            ('invoice_date', '>=', self.date_from),
+            ('invoice_date', '<=', self.date_to),
+        ])
 
-        partners = self.env['res.partner'].search(domain)
+        # Ordini di vendita non annullati nel range
+        orders = SaleOrder.search([
+            ('state', '!=', 'cancel'),
+            ('date_order', '>=', self.date_from),
+            ('date_order', '<=', self.date_to),
+        ])
+
+        partner_data = {}
+
+        # Fatture
+        for inv in invoices:
+            pid = inv.partner_id.id
+            partner_data.setdefault(pid, {'invoices': 0, 'orders': 0, 'indirect': 0})
+            partner_data[pid]['invoices'] += 1
+
+        # Ordini
+        for order in orders:
+            # Cliente principale
+            pid = order.partner_id.id
+            partner_data.setdefault(pid, {'invoices': 0, 'orders': 0, 'indirect': 0})
+            partner_data[pid]['orders'] += 1
+
+            # Cliente indiretto
+            if order.x_studio_cliente_indiretto:
+                ind_pid = order.x_studio_cliente_indiretto.id
+                partner_data.setdefault(ind_pid, {'invoices': 0, 'orders': 0, 'indirect': 0})
+                partner_data[ind_pid]['indirect'] += 1
+
+        # Crea le righe di report
+        for pid, values in partner_data.items():
+            self.env['customer.report.line'].create({
+                'wizard_id': self.id,
+                'partner_id': pid,
+                'invoice_count': values['invoices'],
+                'order_count': values['orders'],
+                'indirect_order_count': values['indirect'],
+            })
 
         return {
-            'name': 'Customers',
-            'view_mode': 'tree,form',
-            'res_model': 'res.partner',
+            'name': 'Clienti Coinvolti',
+            'view_mode': 'tree',
+            'res_model': 'customer.report.line',
             'type': 'ir.actions.act_window',
-            'domain': [('id', 'in', partners.ids)],
+            'domain': [('wizard_id', '=', self.id)],
+            'target': 'new',
         }
