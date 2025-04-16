8dec8548b24db9909719410f31498321cf482e8a
FrancescaBianchini <55796931+FrancescaBianchini@users.noreply.github.com>
2025-04-16 21:48:16 +0200

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Customer Filter Wizard',
+    'version': '1.0',
+    'depends': ['base', 'sale', 'account'],
+    'data': [
+        'views/customer_date_wizard_views.xml',
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
