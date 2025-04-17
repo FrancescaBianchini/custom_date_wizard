02f7ffce5a3251cb47a0e0f074255848cbd60441
FrancescaBianchini <55796931+FrancescaBianchini@users.noreply.github.com>
2025-04-17 22:04:00 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'depends': ['base', 'sale', 'account'],
     'data': [
-        'views/customer_report_line_views.xml',  # prima le viste dei modelli
+        'views/customer_report_line.xml',  # prima le viste dei modelli
         'views/customer_date_wizard_views.xml',  # poi il wizard
     ],
     'installable': True,
